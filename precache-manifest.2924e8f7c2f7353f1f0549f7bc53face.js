ef5c471ea8731c5781931606ff088b91fe0eacc0
kodewalker <51172852+kodewalker@users.noreply.github.com>
2020-08-18 12:01:56 +0530

    Updates

--- precache-manifest.2924e8f7c2f7353f1f0549f7bc53face.js
+++ precache-manifest.2924e8f7c2f7353f1f0549f7bc53face.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fc129c98ae81ca67c28e7815d3abc3a5",
+    "revision": "24a1dbb6d6e6c2ff50bcd761dd32c11d",
     "url": "/bs-assignment/index.html"
   },
   {
-    "revision": "a8f4c00dab524a095cfa",
+    "revision": "f2a43c0576e1aca20e9b",
     "url": "/bs-assignment/static/css/2.33b10ba6.chunk.css"
   },
   {
-    "revision": "617b79a0d886053f1543",
+    "revision": "ab3eca074a0250a11034",
     "url": "/bs-assignment/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "a8f4c00dab524a095cfa",
-    "url": "/bs-assignment/static/js/2.9ad2c0ba.chunk.js"
+    "revision": "f2a43c0576e1aca20e9b",
+    "url": "/bs-assignment/static/js/2.fe5889e1.chunk.js"
   },
   {
     "revision": "f3e5cb1a6b77526442f171e4c0c4c693",
-    "url": "/bs-assignment/static/js/2.9ad2c0ba.chunk.js.LICENSE.txt"
+    "url": "/bs-assignment/static/js/2.fe5889e1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "617b79a0d886053f1543",
-    "url": "/bs-assignment/static/js/main.5a5a2581.chunk.js"
+    "revision": "ab3eca074a0250a11034",
+    "url": "/bs-assignment/static/js/main.f2fdec09.chunk.js"
   },
   {
     "revision": "f2283aa2fa22c4bfc823",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bs-assignment/precache-manifest.3658f9e999d8cf7834ad886ff4f2ce6f.js"
+  "/bs-assignment/precache-manifest.2924e8f7c2f7353f1f0549f7bc53face.js"
 );
 
 self.addEventListener('message', (event) => {
